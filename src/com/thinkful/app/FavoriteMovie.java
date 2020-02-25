f744149277aa7b64ee9aefc4b912d2c9139c43aa
Olivia Davis <olivia.davis593@gmail.com>
2020-02-24 20:55:57 -0600

    implented strings to show favorite movies & actors

---
+++ src/com/thinkful/app/FavoriteMovie.java
@@ -0,0 +1,43 @@
+package com.thinkful.app;
+
+
+public class FavoriteMovie {
+
+    public static void main(String[] args) {
+
+        //movie + actors #1
+        String movieOne = "1. 4 rooms";
+        System.out.println(movieOne);
+
+        String favActorsOne = "Tim Roth & Quentin Tarantino";
+        System.out.println(favActorsOne);
+
+        //movie + actors #2
+        String movieTwo = "2. Lost Boys";
+        System.out.println(movieTwo);
+
+        String favActorsTwo = "Jason Patirc & Corey Feldman";
+        System.out.println(favActorsTwo);
+
+        //movie + actors #3
+        String movieThree = "3. Near Dark";
+        System.out.println(movieThree);
+
+        String favActorsThree = "Bill Paxton & Lance Henriksen";
+        System.out.println(favActorsThree);
+
+        //movie + actors #4
+        String movieFour = "4. The Matrix Reloaded";
+        System.out.println(movieFour);
+
+        String favActorsFour = "Keanu Reeves & Carrie-Anne Moss";
+        System.out.println(favActorsFour);
+
+        //movie + actors #5
+        String movieFive = "5. The Outsiders";
+        System.out.println(movieFive);
+
+        String favActorsFive = "Patrick Swayze & Rob Lowe";
+        System.out.println(favActorsFive);
+    }
+}
